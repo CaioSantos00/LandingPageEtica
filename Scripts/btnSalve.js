0aa3a800e3f08557fc29244877c5c56a71d09e9a
Caio dos Santos Oliveira <caiossantos4321@gmail.com>
2023-09-12 09:30:13 -0300

    Atualização botão salvar

---
+++ Scripts/btnSalve.js
@@ -0,0 +1,6 @@
+let btnSalve = document.getElementById('btnSalve')
+
+btnSalve.addEventListener('click', () => {
+    btnSalve.style.backgroundColor = '#12da5f'
+    btnSalve.innerText = "Salvo"
+})
