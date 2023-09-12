25e4077d7897c5c75bd34ca125b550146a16a35a
Caio dos Santos Oliveira <caiossantos4321@gmail.com>
2023-09-12 08:04:08 -0300

    HUuuuuuuuum aaaaaaaaa

--- login.php
+++ login.php
@@ -16,8 +16,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ética | Cidadania</title>
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/login.css">
     <!--<script src="../js/login.js"></script>-->
 </head>
 <body>
@@ -25,9 +25,9 @@
      <img src="../img/logo.png" id="logo" alt="">
         <nav id='menu-pc'>
         <ul>
-            <li><a href="../index.html#home">Home</a></li>
-            <li><a href="../index.html#publications">Publicações</a></li>
-            <li><a href="../pages/login.php">Login</a></li>
+            <li><a href="index.html#home">Home</a></li>
+            <li><a href="index.html#publications">Publicações</a></li>
+            <li><a href="login.php">Login</a></li>
         </ul>
         </nav>   
     </header>
@@ -40,7 +40,7 @@
                 <input type="text" placeholder="Senha" class="input" name="senha">
                 <button class="btnForms" name="submit">Entrar</button>
                 <a href="" class="textsForms">Esqueceu sua senha ?</a>
-                <a href="cadastroUsuario.html" class="textsForms">Não é um autor ? Cadastre-se</a>
+                <a href="registroUser.php" class="textsForms">Não é um autor ? Cadastre-se</a>
             </form>
         </div>
     </section>
