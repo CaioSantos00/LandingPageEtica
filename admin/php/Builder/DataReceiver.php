65d566dbe601c5a715df4ca7ddda45fdc7aee175
felipeDosProgramas <111545218+felipeDosProgramas@users.noreply.github.com>
2023-09-13 08:54:05 -0300

    esses aí

--- admin/php/Builder/DataReceiver.php
+++ admin/php/Builder/DataReceiver.php
@@ -6,19 +6,26 @@
 		private array $post;
 		
 		function __construct(string $rawPostData){
-			$this->post = json_decode($rawPostData, JSON_OBJECT_AS_ARRAY);			
+			$this->post = json_decode($rawPostData, JSON_OBJECT_AS_ARRAY);
+			print_r($this->post);
 		}
 		
 		function getParsedData() :ParsedData{
 			return new class($this->post) implements ParsedData{
 				private array $Principal;
-				private array $sideArticles;
+				private array $sideArticles = [];
+				private bool $hasArticles = false;
 				
 				function __construct(array $post){
+					if(isset($post[0])){
+					$this->hasArticles 		= true;
 					$this->Principal 		= $post[0];
 					$this->sideArticles 	= $post[1];
+					return;	
+					}
+					$this->Principal 		= $post;					
 				}
-				function get(bool $principal) :array{
+				function get(bool $principal = false) :array{
 					if($principal) return $this->Principal;
 					return $this->sideArticles;
 				}
--- admin/php/View/envio.php
+++ admin/php/View/envio.php
@@ -3,10 +3,14 @@
       $require = '../../../';
       require_once "../Builder/DataReceiver.php";
       require_once "../UserHandler/UserVerify.php";
+      require_once "../Builder/PostSaver..php";
       
       $verifier = new UserVerify("Cookie");
-      $receiver = new DataReceiver('[{"Titulo":"","SemiT":"","Descricao":""},[{"subTitulo":"","descricao":""}]]');      
+      $receiver = new DataReceiver($_POST['data']);      
       $data = $receiver->getParsedData();      
-      print_r($data);
+      //print_r($data);
+      $saver = new PostSaver($data);
+      $saver->saveThem();
+      //PRINT_R($_FILES);
    //}      
 ?>
