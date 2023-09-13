643ac98588e6a6163e039df174bdd14323e87a50
Caio dos Santos Oliveira <caiossantos4321@gmail.com>
2023-09-13 08:46:05 -0300

    Tabela  melhorada

--- Autor/cadastroAdm.php
+++ Autor/cadastroAdm.php
@@ -7,7 +7,7 @@
       <title>Ética | Cidadania</title>
       <link rel="stylesheet" href="../css/style.css" />
       <link rel="stylesheet" href="../css/cadasterAdm.css">
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+     
     </head>
     
     <body>
@@ -46,8 +46,23 @@
         <td>Dado 9</td>
         <td>Dado 10</td>
     </tr>
+    
+    <tr>
+        <td>Dado 6</td>
+        <td>Dado 7</td>
+        <td>Dado 8</td>
+        <td>Dado 9</td>
+        <td>Dado 10</td>
+    </tr>
+    
+    <tr>
+        <td>Dado 6</td>
+        <td>Dado 7</td>
+        <td>Dado 8</td>
+        <td>Dado 9</td>
+        <td>Dado 10</td>
+    </tr>
 </table>
       </section>
     </body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     </html>
